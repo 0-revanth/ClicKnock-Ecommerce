ad05a2a6a0f44bbb43b916dd68e4f6516b10bcb7
0-revanth <168257954+0-revanth@users.noreply.github.com>
2025-08-04 23:15:37 +0530

    The begining

--- ECom/settings.py
+++ ECom/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-@9g7)142ni=w1je$9b^7cl9*1ae(tii=@a_3%uj2-v+w942dbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['clicknock-ecommerce.onrender.com',]
+ALLOWED_HOSTS = ['clicknock-ecommerce.onrender.com', '127.0.0.1', 'localhost']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://clicknock-ecommerce.onrender.com",
+]
 
 
 # Application definition
